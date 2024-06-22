a8e766c216f542175038f55c6c78551fe8931dcf
Patrick Soga <zqe3cg@virginia.edu>
2024-06-21 22:52:53 -0400

    correct download instruction

--- download_lastfm.sh
+++ download_lastfm.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/xiangwang1223/knowledge_graph_attention_network.git;
-cd knowledge_graph_attention_network/data;
-cp lastfm/*.txt ../LightGCN-PyTorch/data;
+cd knowledge_graph_attention_network/Data;
+cp lastfm/*.txt ../../data;
+rm -rf knowledge_graph_attention_network;
